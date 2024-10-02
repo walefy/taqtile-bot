d1e03aae189c77a05ffd36c2c64dd88adb9a2a62
walefy <walefyd1@gmail.com>
2024-10-02 20:54:26 -0300

    feat: implements hash password with scrypt

---
+++ src/services/password-service.ts
@@ -0,0 +1,18 @@
+import { scryptSync, randomBytes, timingSafeEqual } from 'node:crypto';
+import { Buffer } from 'buffer';
+
+export class PasswordService {
+  static hashPassword(password: string) {
+    const salt = randomBytes(16).toString('hex');
+    const buf = scryptSync(password, salt, 64);
+    return `${buf.toString('hex')}.${salt}`;
+  }
+
+  static verifyPassword(planPassword: string, hashedPassword: string) {
+    const [hashedPasswordPartOne, salt] = hashedPassword.split('.');
+    const hashedPasswordBuf = Buffer.from(hashedPasswordPartOne, 'hex');
+    const planPasswordBuf = scryptSync(planPassword, salt, 64);
+
+    return timingSafeEqual(hashedPasswordBuf, planPasswordBuf);
+  }
+}
