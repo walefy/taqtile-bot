082f9025c2a6395e7894f8da33d9d9c1d220d97e
walefy <walefyd1@gmail.com>
2024-10-03 17:29:24 -0300

    chore: configure chai and mocha

---
+++ tests/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+describe('dumb describe', () => {
+  it('dumb test', () => {
+    expect(true).to.be.equal(true);
+  });
+});
